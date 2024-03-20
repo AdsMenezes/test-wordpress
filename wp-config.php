1a81d2c852f40117addd82b891135e744301f351
Anderson Menezes <anderson@codrop.com.br>
2024-03-20 17:15:02 -0300

    chore: add ci/cd3

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'mindxpst_sakata' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'mindxpst_sakata' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'kV5y0wN-_JhD' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
